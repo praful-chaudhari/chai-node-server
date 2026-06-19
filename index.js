89362df7940be1170ce087384a605e1af47c0671
Praful Chaudhari <prafulchaudhari703@gmail.com>
2026-06-19 22:10:37 +0530

    change health check message to hurrayy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT ?? 8000;
 app.get("/", (req, res) => {
     return res.json({
         message: "server is healthy v4.1",
-        status: "Github Actions",
+        status: "Github Actions Hurrayyy",
     });
 });
 
